62950dcd7c8c684ec6ad2112b8c43967993e6a8c
surajbokadia1106-hue <surajbokadia1106@gmail.com>
2026-09-21 01:54:41 +0530

    sw.js

---
+++ sw.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (e) => {
+  // Passes network requests directly
+  e.respondWith(fetch(e.request));
+});
